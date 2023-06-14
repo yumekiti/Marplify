2ff7593bf96f4fc11b073ca5960c015d9145736d
yuyakan <kanbe1365@icloud.com>
2023-06-14 16:30:51 +0900

    feat: 編集とプレビュー切り替えボタンの実装

--- src/components/ModeSwitchBar.tsx
+++ src/components/ModeSwitchBar.tsx
@@ -1,16 +1,31 @@
+import { Mode } from "../App";
 import Card from "./Card";
 
 type ModeSwitchBarPropsType = {
-   
+    mode:  Mode
+    handleModeSwitch: (mode: Mode) => void
 }
 
-const ModeSwitchBar = (props: ModeSwitchBarPropsType) => {
+const ModeSwitchBar = ({ mode, handleModeSwitch }: ModeSwitchBarPropsType) => {
+
+    const onClick = (selectedMode: Mode) => {
+        if (mode != selectedMode) {
+            handleModeSwitch(selectedMode)
+        }
+    }
+
     return (
         <Card>
             <div className='flex justify-between items-center gap-2'>
-              <div className='w-10 h-10 bg-gray-300 rounded-full'></div>
-              <div className='w-10 h-10 bg-gray-300 rounded-full'></div>
-              <div className='w-10 h-10 bg-gray-300 rounded-full'></div>
+                <div className='w-10 h-10 bg-gray-300 rounded-full'>
+                    <button onClick={() => onClick(Mode.Edit)}>Edit</button>
+                </div>
+                <div className='w-10 h-10 bg-gray-300 rounded-full'>
+                    <button onClick={() => onClick(Mode.Both)}>Both</button>
+                </div>
+                <div className='w-10 h-10 bg-gray-300 rounded-full'>
+                    <button onClick={() => onClick(Mode.Preview)}>Preview</button>
+                </div>
             </div>
         </Card>
     );
