8d80ed199a7e32f9118ec1ed3232146a5cf443d6
yumekiti <yumekiti120440@gmail.com>
2023-06-24 09:26:26 +0900

    `feat` redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { FC } from 'react';
 import MainPage from './components/pages/MainPage';
 import PresentationPage from './components/pages/PresentationPage';
 
+import shortid from 'shortid';
+
 const App: FC = () => {
+  const uuid = shortid.generate();
+
   return (
     <div className='App h-screen'>
       <BrowserRouter>
         <Switch>
           <Route exact path='/'>
+            <Redirect to={`/${uuid}`} />
+          </Route>
+          <Route exact path='/:uuid'>
             <MainPage />
           </Route>
-          <Route exact path='/presentation'>
+          <Route exact path='/:uuid/presentation'>
             <PresentationPage />
           </Route>
         </Switch>
