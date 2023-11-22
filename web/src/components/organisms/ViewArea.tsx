4da088eb80d02ef1337f852f690ccdce20a59dbe
yumekiti <yumekiti120440@gmail.com>
2023-11-22 22:15:30 +0900

    fix: mermaid setup classname

--- web/src/components/organisms/ViewArea.tsx
+++ web/src/components/organisms/ViewArea.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { useSelector } from 'react-redux';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
@@ -12,11 +12,6 @@ import SetStyle from '../molecules/SetStyle';
 
 const Component: FC = () => {
   const { content, theme } = useSelector((state: RootState) => state.content);
-
-  useEffect(() => {
-    mermaid.initialize({ startOnLoad: true });
-    mermaid.run({ querySelector: '.language-mermaid' });
-  }, [content]);
 
   return (
     <div className='w-full relative bg-cardBackground overflow-y-scroll h-full bg-cardBackground rounded-lg px-4 py-2 shadow-md'>
