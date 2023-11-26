2438f434df0c494ff2ab21865a3826cdd7e3f301
yumekiti <yumekiti120440@gmail.com>
2023-11-26 17:06:32 +0900

    refactor: format code

--- web/src/components/organisms/Sidebar.tsx
+++ web/src/components/organisms/Sidebar.tsx
@@ -3,9 +3,13 @@ import UserPanel from '../molecules/UserPanel';
 
 const Component = () => {
   return (
-    <div className='bg-cardBackground w-1/6 p-4 h-full border-r border-subheadline'>
+    <div className='bg-cardBackground w-1/6 p-4 h-full border-r border-subheadline relative'>
       <UserPanel />
       <Navigation />
+      {/* 右側のボーダーの中央に閉じるボタンを配置 */}
+      <div className='absolute -right-3 top-1/2 transform -translate-y-1/2'>
+        <button className='text-subheadline'>閉じる</button>
+      </div>
     </div>
   );
 };
