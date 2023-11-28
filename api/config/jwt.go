20b3faff88ae71df97a8a175b6cde6e40e62d6e7
yumekiti <yumekiti120440@gmail.com>
2023-11-28 18:13:35 +0900

    chore: add new files and functions for user authentication

---
+++ api/config/jwt.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"github.com/golang-jwt/jwt"
+
+	"api/domain"
+	"time"
+)
+
+type JwtCustomClaims struct {
+	jwt.StandardClaims
+}
+
+var JwtSecret = []byte("secret")
+
+func GenerateToken(user *domain.User) (string, error) {
+	claims := JwtCustomClaims{
+		*user,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(JwtSecret)
+}
