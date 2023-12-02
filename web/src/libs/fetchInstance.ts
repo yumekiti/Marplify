012ab9997d50e0f0d29d1574db82a05d5f0357c4
yumekiti <yumekiti120440@gmail.com>
2023-12-02 22:55:18 +0900

    feat: update docker-compose.yml and add nginx

--- web/src/libs/fetchInstance.ts
+++ web/src/libs/fetchInstance.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export const fetchInstance = () => {
+  if (typeof window === 'undefined') return axios.create();
   return axios.create({
-    baseURL: 'https://marplify.yumekiti.net/api/',
+    baseURL: `${location.origin}/api`,
     // baseURL: 'http://localhost:8000/',
     headers: {
       'Content-Type': 'application/json',
