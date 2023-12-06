781e38a509a4d6b20ff94b9824f2e59d71f35fdd
yumekiti <yumekiti120440@gmail.com>
2023-12-06 17:34:10 +0900

    refactor: header component

--- web/src/components/organisms/Header.tsx
+++ web/src/components/organisms/Header.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
 
 import icon from '../../assets/icon.svg';
 
@@ -8,10 +7,10 @@ const Component: FC = () => {
     <header className='bg-cardBackground'>
       <div className='container mx-auto px-4 py-2'>
         <div className='flex items-center justify-between'>
-          <Link to='/' className='flex items-center ml-2'>
+          <div className='flex items-center ml-2'>
             <img src={icon} alt='icon' className='w-10' />
             <p className='text-xl font-bold ml-2 tracking-widest'>Marplify</p>
-          </Link>
+          </div>
         </div>
       </div>
     </header>
